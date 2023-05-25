d745251fcf4e8b3a753bb90b3341cbab8f8e27b8
danthecm <etchiedaniel1@gmail.com>
2023-05-25 17:31:49 +0100

    2: task 0: changed host and added strict_slash

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)

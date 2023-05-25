44f1e3b6c806d4e3bd9ac23a6ce6641a89f56576
danthecm <etchiedaniel1@gmail.com>
2023-05-25 18:10:59 +0100

    1: web framework task 3: python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A simple flask server with few routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    The index route returning Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    The hbnb route returning HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    C route that returns C and the text
+    """
+    new_text = text.replace("_", " ")
+    return f"C {new_text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    Python route prints python and text
+    """
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

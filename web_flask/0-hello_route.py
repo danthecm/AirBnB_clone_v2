24d8fe4a5ea7ea6bc840338a8b3688e55890d995
danthecm <etchiedaniel1@gmail.com>
2023-05-25 17:35:37 +0100

    4: task 0: make file executable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """
     The index route returning Hello HBNB!
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

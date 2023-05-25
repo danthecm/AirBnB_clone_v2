5678ba8b6bab3ae88e599ca6fd750fbce276e41b
danthecm <etchiedaniel1@gmail.com>
2023-05-25 17:43:13 +0100

    1: web framework task 1: added hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A simple flask server model with one route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    The index route returning Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    The hbnb route returning HBNB
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

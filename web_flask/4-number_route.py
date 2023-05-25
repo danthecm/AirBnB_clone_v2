16e5d56a82af5eeb5bcf2c7c6a1e9bba4308fbac
danthecm <etchiedaniel1@gmail.com>
2023-05-25 22:23:02 +0100

    1: web framework task 4: number only

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+A simple flask server model with one route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    The index route returning Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    The hbnb route returning HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    C route that returns C and the text
+
+    Args:
+    text: a string to add to C and print
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    Python route prints python and text
+
+    Args:
+    text: a string to add to python
+    """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Number route prints the number 
+
+    Args:
+    Number: a number to print
+    """
+    return n + " is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
